6fb968be87a28e2a0bcd6a7e5bd817862a1a1546
DinoSehic <dino.sehic@syntio.net>
2021-07-02 14:00:50 +0200

    add mistakes

--- main/main.go
+++ main/main.go
@@ -12,11 +12,15 @@ func main() {
 	var x int
 	fmt.Scanf("%d\n", &x)
 	fmt.Println()
-
+  
 	fmt.Print("Enter the second number: ")
 	var y int
 	fmt.Scanf("%d\n", &y)
 	fmt.Println()
 
 	fmt.Print("Sum of ", x, " and ", y, " is: ", functions.Sum(x, y))
+}
+
+func mul(a, b int) int {
+	return a * b
 }
